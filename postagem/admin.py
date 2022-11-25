390db1226bd26c4d8d46096335bbd9ac1c925858
Forrageador <forrageador@gmail.com>
2022-11-24 23:51:45 -0300

    commit

--- postagem/admin.py
+++ postagem/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display=('id','nome')
+    list_display=('id','nomeProduto')
 
 # Register your models here.
 admin.site.register(Produto,ProdutoAdmin)
--- postagem/models.py
+++ postagem/models.py
@@ -8,11 +8,12 @@ class Categoria(models.Model):
         return self.nome
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=140)
+    nomeProduto = models.CharField(max_length=140)
     descricao = models.TextField()
-    valor = models.DecimalField(max_digits=8,decimal_places=2)
+    valor_produto = models.DecimalField(max_digits=8,decimal_places=2)
     categoria = models.ManyToManyField(Categoria)
     imagem = models.ImageField(upload_to='produtos/', blank=True, null=True, max_length=250)
 
     def __str__(self):
         return self.nome
+
